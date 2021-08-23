82a04ee40c01c5cc880400afeff641956a104cb3
Andres Becerra <andresbecerra7200@gmail.com>
2021-08-23 18:54:14 -0500

    Updated

--- app/src/main/java/app/absoftware/usersgrid/services/RequestQueueSingleton.kt
+++ app/src/main/java/app/absoftware/usersgrid/services/RequestQueueSingleton.kt
@@ -5,12 +5,17 @@ import com.android.volley.RequestQueue
 import com.android.volley.toolbox.Volley
 
 
+
+// una de las caracteristicas del patron es crear el contructor privado
 class RequestQueueSingleton private constructor(private var context: Context) {
+
     private var requestQueue: RequestQueue?
+
     fun getRequestQueue(): RequestQueue? {
         if (requestQueue == null) {
             requestQueue = Volley.newRequestQueue(context!!.getApplicationContext())
         }
+        // retorna este objeto cuando ya hasido instanciado
         return requestQueue
     }
 
@@ -21,6 +26,7 @@ class RequestQueueSingleton private constructor(private var context: Context) {
             if (requestQueueSingleton == null) {
                 requestQueueSingleton = RequestQueueSingleton(context)
             }
+            // retorna este objeto cuando ya hasido instanciado
             return requestQueueSingleton
         }
     }
--- app/src/main/java/app/absoftware/usersgrid/services/VolleyCallback.kt
+++ app/src/main/java/app/absoftware/usersgrid/services/VolleyCallback.kt
@@ -1,5 +1,6 @@
 package app.absoftware.usersgrid.services
 
+// una interface para usar el callback
 interface VolleyCallback {
     fun onSuccessResponse(result: Any?)
     fun onErrorResponse(error: String?)
